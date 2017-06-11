b12fc85e272cc32d6d629975a6a0de903f2b9176
Jason Holloway <miceeatnicerice@hotmail.com>
2017-06-11 02:13:42 +0100

    Game states kind of working

--- src/bowling.ts
+++ src/bowling.ts
@@ -1,58 +1,145 @@
 
-abstract class GameState {
+abstract class State {
+   parent: State;
+   prev: State;
+
+   constructor(parent: State, prev: State) {
+      this.parent = parent;
+      this.prev = prev;
+   }
+
+   receive(state: State): State {
+      throw new Error('Can\'t receive state here!');
+   }
+}
+
+
+
+class Game extends State {
+   frameCount: number;
+   frameSize: number;
+
+   constructor(frameCount: number, frameSize: number) {
+      super(undefined, undefined);
+      this.frameCount = frameCount;
+      this.frameSize = frameSize;
+   }
+
+   begin(): Frame {        
+      return new Frame(this, undefined, 0, this.frameSize);
+   }
+
+   receive(frameResult: FrameResult): State {
+      const frameIndex = frameResult.frame.index;
+
+      if(frameIndex + 1 < this.frameCount) {
+         return new Frame(this, frameResult, frameIndex + 1, this.frameSize);         
+      }
+      else {
+         return new GameResult(this, frameResult);
+      }
+   }
+}
+
+class GameResult extends State {
+   constructor(parent: Game, prev: State) {
+      super(parent, prev);
+   }
+}
+
+
+class Frame extends State {
+   index: number;
+   turnCount: number;
+
+   constructor(parent: State, prev: State, index: number, turnCount: number) {
+      super(parent, prev);
+      this.index = index;
+      this.turnCount = turnCount;
+   }
+
+   begin(): Turn {
+      return new Turn(this, this, 0);
+   }
+
+   receive(turnResult: TurnResult): State {      
+      const turnIndex = turnResult.turn.index;
+
+      if(turnIndex + 1 < this.turnCount) {
+         return new Turn(this, turnResult, turnIndex + 1);
+      }
+      else {
+         return new FrameResult(this.parent, turnResult, this);
+      }
+   }
 
 }
 
-class Game implements GameState {
-    constructor() {}
+class FrameResult extends State {
+   frame: Frame;
 
-    beginFrame(): Frame {
-        return null;
-    }
+   constructor(parent: State, prev: State, frame: Frame) {
+      super(parent, prev);
+      this.frame = frame;
+   }
+
+   continue(): State {
+      return this.parent.receive(this);
+   } 
 }
 
-class Frame implements GameState {
-    constructor() {}
 
-    rollTurn(downedPins: number): Turn {
-        return null;
-    }
+class Turn extends State {
+   index: number;
+
+   constructor(parent: Frame, prev: State, index: number) {
+      super(parent, prev);
+      this.index = index;
+   }
+
+   roll(hitCount: number): TurnResult {
+      return new TurnResult(this.parent, this, hitCount);
+   }
+   
+   receive(result: TurnResult): State {
+      return this.parent.receive(result);
+   }
 }
 
-class Turn implements GameState {
-    constructor() {}
+class TurnResult extends State {
+   turn: Turn;
+   hitCount: number;
+
+   constructor(parent: State, prev: Turn, hitCount: number) {
+      super(parent, prev);
+      this.turn = prev;
+      this.hitCount = hitCount;
+   }
+
+   continue(): State {
+      return this.parent.receive(this);
+   }
 }
+
+
+
 
 
 
 class Player {    
-    score: number;
+   score: number;
 
-    proceed(ctx: GameState): GameState {
-        if(ctx instanceof Game) {
-            console.log('Game!');
-            return ctx;
-        }
-        else if(ctx instanceof Frame) {
-            console.log('Frame!');
-            return ctx;
-        }
+   proceed(ctx: State): State {
+      if(ctx instanceof Game) {
+         return ctx;
+      }
+      else if(ctx instanceof Frame) {
+         return ctx;
+      }
 
-        throw new Error('Strange GameState provided!');
-    }
+      throw new Error('Strange State provided!');
+   }
 }
 
 
-
-function welcome() {    
-
-    const player = new Player();
-
-    player.proceed(new Frame());
-    player.proceed(new Game());
-    player.proceed(new Frame());
-}
-
-export { GameState, Game, Frame, Turn, Player }
-
-export default welcome;
+export { State, Game, GameResult, Frame, FrameResult, Turn, TurnResult, Player }
--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,67 @@
-import {expect} from 'chai';
-import {GameState, Game, Frame, Turn, Player} from '../src/bowling';
+import {gen, property} from 'testcheck';
+import {check, install as mochaTestcheckInstall} from 'mocha-testcheck';
+mochaTestcheckInstall();
 
-describe('something', () => {
-    it('yup', () => {
-        let player = new Player();
+import {expect, assert} from 'chai';
+import {State, Game, GameResult, Frame, FrameResult, Turn, TurnResult, Player} from '../src/bowling';
 
-        expect(13).to.eq(13);
-    });
+
+function play(state: State): State {
+   if(state instanceof Game) {
+      return (state as Game).begin();
+   }
+   else if(state instanceof Frame) {
+      return (state as Frame).begin();
+   }   
+   else if(state instanceof Turn) {
+      return (state as Turn).roll(0);
+   }
+   else if(state instanceof TurnResult) {
+      return (state as TurnResult).continue();
+   }
+   else if(state instanceof FrameResult) {
+      return (state as FrameResult).continue();
+   }
+}
+
+const genFrame = gen.array( [gen.intWithin(0, 8), gen.intWithin(1, 10)] )
+                     .then(( [index, turnCount] ) => new Frame(null, null, index, turnCount));
+
+
+describe('Frame', () => {
+   check.it('allows so many turns before yielding a FrameResult', 
+      genFrame,
+      (frame: Frame) => {
+         let turnsPlayed = 0;
+         let state = frame as State;
+
+         while(true) {
+            state = play(state);
+            if(state instanceof Turn) turnsPlayed++;
+            if(state instanceof FrameResult) break;
+         }
+
+         expect(turnsPlayed).to.equal(frame.turnCount);
+      });
+});
+
+
+const genGame = gen.array([gen.sPosInt.scale(s => s / 10), gen.sPosInt.scale(s => s / 100)])
+                     .then(([frameCount, frameSize]) => new Game(frameCount, frameSize));
+
+describe('Game', () => {
+   check.it('allows so many frames before yielding a GameResult', 
+      genGame,
+      (game: Game) => {
+         let framesPlayed = 0;
+         let state = game as State;
+
+         while(true) {
+            state = play(state);
+            if(state instanceof FrameResult) framesPlayed++;
+            if(state instanceof GameResult) break;
+         }
+
+         expect(framesPlayed).to.equal(game.frameCount);
+      });
 });
